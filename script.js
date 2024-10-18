1855e6fb68fd3a5b302426ed23ed679212d947b3
Mateus Martini <mateusrmartini@gmail.com>
2024-10-18 13:35:31 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+window.onload = function () {
+  const lines = [
+    'console.log("Bem-vindo ao meu portfólio!");',
+
+    'let skills = ["Red Team | ", "Pentesting ", "Cybersecurity", "Developer Software" ];',
+    'document.getElementById("Inicio").style.display = "block";',
+  ];
+
+  lines.forEach((line, index) => {
+    setTimeout(() => {
+      const codeLine = document.createElement("div");
+      codeLine.className = "code-line";
+      codeLine.textContent = line;
+      document.querySelector(".code-container").appendChild(codeLine);
+    }, index * 2000); // Atraso de 2 segundos entre as linhas
+  });
+};
+
+//HABILIDADES
+// Seleciona a seção de habilidades
+
+document.addEventListener("scroll", function () {
+  const habilidadesSection = document.getElementById("habilidades");
+  const sectionPosition = habilidadesSection.getBoundingClientRect().top;
+  const screenPosition = window.innerHeight;
+
+  // Verifica se a seção está visível na tela
+  if (sectionPosition < screenPosition) {
+    habilidadesSection.classList.add("visible"); // Adiciona a classe visible
+    habilidadesSection.classList.remove("hidden"); // Remove a classe hidden
+  }
+});
+
+//Projetos
+
+document.addEventListener("scroll", function () {
+  const projetosSection = document.getElementById("projetos");
+  const sectionPosition = projetosSection.getBoundingClientRect().top;
+  const screenPosition = window.innerHeight;
+
+  // Verifica se a seção está visível na tela
+  if (sectionPosition < screenPosition) {
+    projetosSection.classList.add("visible"); // Adiciona a classe visible
+    projetosSection.classList.remove("hidden"); // Remove a classe hidden
+  }
+});
+
+const radioButtons = document.querySelectorAll("input[type=radio]");
+
+const slideShow = () => {
+  for (let i = 0; i < radioButtons.length; i++) {
+    setInterval(() => {
+      console.log(i);
+    }, 3000);
+  }
+};
+
+slideShow();
+
+console.log("Hello");
+
+//Terminal
